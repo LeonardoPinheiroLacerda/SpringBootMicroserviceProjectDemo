6e90d877373d9c2126c74788e9a56007514cb449
LeonardoPinheiroLacerda <leon.lacerda2015@gmail.com>
2023-01-28 19:07:09 -0300

    Customer connected to a database

--- customer/src/main/java/com/leonardo/customer/CustomerApplication.java
+++ customer/src/main/java/com/leonardo/customer/CustomerApplication.java
@@ -3,16 +3,11 @@ package com.leonardo.customer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.leonardo.shared.dto.FooDTO;
-
 @SpringBootApplication
 public class CustomerApplication {
-    
-    public static void main(String [] args) {
-        SpringApplication.run(CustomerApplication.class, args);
 
-        FooDTO foo = new FooDTO("aa", 0, 3.1f, "value4", false);
-        System.out.println(foo);
-    }
-
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerApplication.class, args);
+	}
+	
 }
